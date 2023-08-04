ab58857b506feb3b8ddae612cbf73789ff533f61
Cix <gabrielviniciodacosta@gmail.com>
2023-08-04 15:06:04 -0300

    '

--- js/script.js
+++ js/script.js
@@ -5,22 +5,14 @@ const braco = document.querySelector("#braco")
 const controle = document.querySelectorAll(".controle-ajuste")
 
 controle.forEach((elemento) => {
-    elemento.addEventListener("click", () => {
-        console.log("funcionou")
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
     })   
 })
-subtrair.addEventListener("click", () => {
-    { manipulaDados("subtrair") }
-    braco.value = parseInt(braco.value);
-});
 
-somar.addEventListener("click", () => {
-    { manipulaDados("somar") }
-    braco.value = parseInt(braco.value);
-});
 
 function manipulaDados(operacao) {
-    if (operacao === "subtrair") {
+    if (operacao === "-") {
         braco.value = parseInt(braco.value) - 1
 
     } else {
