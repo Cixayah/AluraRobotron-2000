07926b43568c95d6d31c3245dfe29339b0a6256e
Cix <gabrielviniciodacosta@gmail.com>
2023-08-04 14:31:43 -0300

    a

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
 const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
-const robotron = document.querySelector("#robotron")
 
 somar.addEventListener("click", (evento) => {
     braco.value = parseInt(braco.value) + 1;
-
-
 });
 
 subtrair.addEventListener("click", (evento) => {
     braco.value = parseInt(braco.value) - 1;
-
-
 });
