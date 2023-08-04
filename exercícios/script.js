affe3dbd2fa45369c5968f2b15ae942ba4b270f1
Cix <gabrielviniciodacosta@gmail.com>
2023-08-04 15:27:33 -0300

    '

--- exercícios/script.js
+++ exercícios/script.js
@@ -1,3 +1,11 @@
+
+var lista = ["Laranja", "Vermelho", "Branco", "Amarelo", "Rosa"];
+
+lista.splice(1,1)
+
+console.log(lista);
+
+
 const robotron = document.querySelector("#calcular")
 
 robotron.addEventListener("click", (fuiClickado) => {
